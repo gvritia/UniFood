0a893174df354cb33419ab8b5c871f639fb24e6b
user <madmax1080pps@gmail.com>
2026-03-05 10:17:41 +0300

    solve problem with load .env now server is working

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,18 +1,18 @@
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_DIR / ".env"
 # загружает .env
-try:
-    load_dotenv(encoding='utf-8')
-except UnicodeDecodeError:
-    try:
-        load_dotenv(encoding='cp1251')  # для Windows
-    except UnicodeDecodeError:
-        load_dotenv(encoding='latin-1')  # запасной вариант
+load_dotenv(dotenv_path=ENV_PATH)
+# load_dotenv(encoding='utf-8')
+# запасной вариант
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
+print(DATABASE_URL)
 try:
     ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 except (TypeError, ValueError):
